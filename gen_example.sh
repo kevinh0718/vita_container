d5374c572890765c382a9413e85ded2177c35026
Kevin Huang <hkhuang3@ohm.bioen.illinois.edu>
2025-10-07 16:49:28 -0500

    minor update

--- gen_example.sh
+++ gen_example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd vita_example 
-ccmake .
-# Use the ccmake interface to configure the project (press c, and one more c, then g).
+cmake .
+# If using the ccmake, then configure the project with pressing c, and one more c, then g, to generate the Makefile.
 make
 ./example_1
