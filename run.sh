f1daf6aaea69e8fe877b53f247785a46b579e6e7
Kevin Huang <hkhuang3@ohm.bioen.illinois.edu>
2025-10-07 16:48:07 -0500

    minor update

--- run.sh
+++ run.sh
@@ -2,16 +2,28 @@
 
 # This script automates the process of running the VItA container
 
-# Check if the data directory exists
-if [ ! -d "vita_example" ]; then
-  echo "Error: The 'vita_example' directory was not found."
+#
+# -----------------------------------------------
+# UPDATE THESE VARIABLES ONLY
+# -----------------------------------------------
+TARGET_FOLDER="vita_example"
+SCRIPT_NAME="gen_example.sh"
+# -----------------------------------------------
+# DO NOT EDIT BELOW THIS LINE
+# -----------------------------------------------
+#
+
+# Check if the data directory exists using the variable
+if [ ! -d "$TARGET_FOLDER" ]; then
+  echo "Error: The '$TARGET_FOLDER' directory was not found."
   echo "Please make sure you are running this script from the root of the project."
   exit 1
 fi
 
-# Run the Docker container with the volume mount
+# Run the Docker container with the volume mounts
+# Both volume paths are now constructed using the variables
 docker run -it --rm \
-  -v "$(pwd)/vita_example":/app/vita_example \
-  -v "$(pwd)/run_example.sh":/app/run_example.sh \
+  -v "$(pwd)/$TARGET_FOLDER":/app/"$TARGET_FOLDER" \
+  -v "$(pwd)/$SCRIPT_NAME":/app/"$SCRIPT_NAME" \
   kevinh0718/vita_talou_cco:latest \
-  bash /app/run_example.sh
+  bash /app/"$SCRIPT_NAME"
